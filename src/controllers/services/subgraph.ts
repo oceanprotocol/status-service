3e15a9c4f34f58d721b9f6c64d70b2a4dc485dba
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-09-13 16:39:44 +0100

    Checking sub-graph block number

--- src/controllers/services/subgraph.ts
+++ src/controllers/services/subgraph.ts
@@ -2,6 +2,7 @@ import fetch from 'cross-fetch'
 import { Network } from '../../@types'
 import getWeb3Provider from '../utils/ethers'
 import latestRelease from '../utils/github'
+import ethers from 'ethers'
 
 async function subgraphFetch(network: string, query: string) {
   const response = await fetch(
@@ -16,18 +17,19 @@ async function subgraphFetch(network: string, query: string) {
 }
 
 export default async function subgraphStatus(network: Network) {
-  const web3Provider = getWeb3Provider(network)
-  const blockNum = await web3Provider.getBlockNumber()
-  console.log('\n\n\n### BLOCK:', blockNum)
-
   const query = `{globalStatistics{version} _meta{ block { number } }}`
   const response = await subgraphFetch(network.name, query)
   const data = (await response.json()).data
 
-  console.log(process.env.BLOCK_TOLERANCE)
-  console.log(Number(process.env.BLOCK_TOLERANCE))
-  const blockCheck = data._meta.number + Number(process.env.BLOCK_TOLERANCE)
-  console.log('blockCheck', blockCheck)
+  const blockCheck =
+    data._meta.block.number + Number(process.env.BLOCK_TOLERANCE)
+  let web3Provider: ethers.providers.Provider
+  let blockNum: number
+
+  if (network.name && network.infuraId) {
+    web3Provider = getWeb3Provider(network)
+    blockNum = await web3Provider.getBlockNumber()
+  }
 
   const version = data.globalStatistics[0].version
   const release = await latestRelease('ocean-subgraph')
--- src/controllers/utils/ethers.ts
+++ src/controllers/utils/ethers.ts
@@ -1,7 +1,9 @@
 import { ethers } from 'ethers'
 import { Network } from '../../@types'
 
-export default function getWeb3Provider(network: Network) {
+export default function getWeb3Provider(
+  network: Network
+): ethers.providers.Provider {
   if (!network.name || !network.infuraId) return
   if (network.name === 'polygon') network.name = 'matic'
   if (network.name === 'mumbai') network.name = 'maticmum'
