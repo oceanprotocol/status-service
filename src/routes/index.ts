25a3e15dfa0d8256cae91f7e51ec18ea6a1480a2
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-10-05 17:21:43 +0300

    Removing routes for requesting data

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,24 +1,8 @@
 import { Request, Response } from 'express'
 import express from 'express'
 import monitor from '../controllers'
-import { networkStatus, getStatus } from '../controllers/db'
-import { Status } from '../@types'
 
 const router = express.Router()
-
-/* GET: current status of Ocean components on all networks. */
-router.get('/', async function (req: Request, res: Response) {
-  await getStatus((rows: Status[]) => {
-    res.send(rows)
-  })
-})
-
-/* GET: current status of Ocean components on a given network. */
-router.get('/network/:network', async function (req: Request, res: Response) {
-  await networkStatus(req.params.network, (row: Status) => {
-    res.send(row)
-  })
-})
 
 /* GET: trigger the monitoring of all Ocean components. */
 router.get('/forceUpdate', async function (req: Request, res: Response) {
